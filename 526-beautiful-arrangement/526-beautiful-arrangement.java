63a9d36b1186dd969cdb6ac2886569a5bb07b1a2
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2022-09-25 09:09:11 -0700

    Time: 245 ms (10.76%), Space: 65.2 MB (5.78%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -3,19 +3,15 @@ class Solution {
     public int countArrangement(int n) {
         res = 0;
         HashSet<Integer> map = new HashSet<>();
-        List<Integer> arr = new ArrayList<>();
-        helper(n, 1, map, arr);
+        helper(n, 1, map);
         
         return res;
     }
     
-    private void helper(int n, int index, HashSet<Integer> map, List<Integer> arr) {
+    private void helper(int n, int index, HashSet<Integer> map) {
         // base
         if(index == n+1) {
             res++;
-            // for(int i : arr) {
-            //     System.out.println(i);
-            // }
             return;
         }
         
@@ -24,11 +20,8 @@ class Solution {
         for(int i = 1;  i <= n; i++) {
             if((i % index == 0 || index % i == 0) && !map.contains(i)) {
                 map.add(i);
-                arr.add(i);
-                // System.out.println(i);
-                helper(n, index+1, map, arr);
+                helper(n, index+1, map);
                 map.remove(i);
-                arr.remove(arr.size()-1);
             }
         }
         
