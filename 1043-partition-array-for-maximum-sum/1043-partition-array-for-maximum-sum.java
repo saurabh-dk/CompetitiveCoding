8baeb0bb4c71046d3f6448e0f01972632e315f45
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2022-10-25 20:45:21 -0700

    Time: 14 ms (36.79%), Space: 42.1 MB (81.09%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,13 +1,33 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] A, int K) {
-        int N = A.length, dp[] = new int[N];
-        for (int i = 0; i < N; ++i) {
-            int curMax = 0;
-            for (int k = 1; k <= K && i - k + 1 >= 0; ++k) {
-                curMax = Math.max(curMax, A[i - k + 1]);
-                dp[i] = Math.max(dp[i], (i >= k ? dp[i - k] : 0) + curMax * k);
+        int[] memo = new int[A.length];
+        memo[0] = A[0];
+        int initMax = A[0];
+
+        for(int i = 1; i < K; ++i) {
+            if (A[i] > initMax) {
+                initMax = A[i];
+            }
+            memo[i] = (i+1) * initMax;
+        }
+
+        for (int i = K; i < A.length; ++i) {
+            int cur = 0;
+            int kIntervalMax = A[i];
+			// Backtrack up to K-1 indices to calculate current maximum for memo[i].
+            for (int j = 1; j <= K; ++j) {
+			    // Keep track of the current maximum in the window [i-j+1, i].
+                if (A[i-j+1] > kIntervalMax) {
+                    kIntervalMax = A[i-j+1];
+                }
+				// cur is the candidate for the solution to memo[i] as we backtrack the K-1 window.
+                cur = memo[i-j] + j * kIntervalMax;
+
+                if (cur > memo[i]) {
+                    memo[i] = cur;
+                }
             }
         }
-        return dp[N - 1];
+        return memo[A.length-1];
     }
 }
